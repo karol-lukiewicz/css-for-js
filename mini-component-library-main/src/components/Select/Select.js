9bce0db2b616b20b1c8a661fe4564c0be6cb1da8
Karol Łukiewicz <karol.lukiewicz@gmail.com>
2024-01-23 22:47:10 +0100

    module3: workdshop - select

--- mini-component-library-main/src/components/Select/Select.js
+++ mini-component-library-main/src/components/Select/Select.js
@@ -1,18 +1,68 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <SelectWrapper>
+      <SelectElement value={value} onChange={onChange}>
+        {children}
+      </SelectElement>
+      <SelectFront>
+        {displayedValue}
+        <Icon id="chevron-down" strokeWidth={2} size={16}></Icon>
+      </SelectFront>
+    </SelectWrapper>
   );
 };
+
+const SelectElement = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`;
+
+const SelectWrapper = styled.div`
+  width: max-content;
+  height: 43px;
+  position: relative;
+
+  background: ${COLORS.transparentGray15};
+  border-width: 0;
+  border-radius: 8px;
+  padding: 12px 10px 12px 16px;
+
+  color: ${COLORS.gray700};
+  font-family: Roboto;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 19px;
+  letter-spacing: 0em;
+  text-align: left;
+
+  &:has(${SelectElement}:focus) {
+    outline: 1px dotted #212121;
+    outline: 2px auto -webkit-focus-ring-color;
+  }
+
+  &:has(${SelectElement}:hover) {
+    color: ${COLORS.black};
+  }
+`;
+
+const SelectFront = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+  align-items: center;
+  gap: 18px;
+`;
 
 export default Select;
